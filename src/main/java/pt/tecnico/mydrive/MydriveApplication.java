8cc14b15e3482e395fa25a36ad81b26d1f3b084b
sikrew <gomesgil@bitbucket.com>
2016-02-25 17:27:12 +0000

    mydrive main class stub

---
+++ src/main/java/pt/tecnico/mydrive/MydriveApplication.java
@@ -0,0 +1,24 @@
+package pt.tecnico.mydrive;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+public class MydriveApplication {
+    static final Logger log = LogManager.getRootLogger();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("*** Welcome to the MyDrive application! ***");
+	}
+}
