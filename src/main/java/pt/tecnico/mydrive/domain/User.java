74565144ea5787d283c56e8f1dd20cd53895b2b2
FonsecaGitHub <miguel.fonseca1011011@gmail.com>
2016-03-07 09:23:24 +0000

    User class

---
+++ src/main/java/pt/tecnico/mydrive/domain/User.java
@@ -0,0 +1,45 @@
+package pt.tecnico.mydrive.domain;
+
+import pt.tecnico.mydrive.domain.Permissions;
+import pt.tecnico.mydrive.domain.File;
+import pt.tecnico.mydrive.exception.InvalidUsernameException;
+import pt.tecnico.mydrive.exception.SpreadSheetDoesNotExistException;
+import pt.tecnico.mydrive.exception.UserDoesNotHaveAccessException;
+
+import org.jdom2.DataConversionException;
+import org.jdom2.Element;
+
+public class User extends User_Base {
+	
+	public User() {
+		super();
+	}
+
+	public User(String username, String name) throws InvalidUsernameException {
+		super();
+		if (username.length() < 1) {
+			throw new InvalidUsernameException(username);
+		}
+		setUsername(username);
+		setName(name);
+	}
+
+	public void delete() {
+		//setMyDrive(null);
+		//adicionar funcao em Permissions
+		/*for (Permissions permissions : getPermissionsSet()) {
+			permissions.getFile().delete();
+		}*/
+		deleteDomainObject();
+	}
+
+	//TODO
+	public Element exportToXML() {
+
+	}
+
+	//TODO
+	public void importFromXML() {
+
+	}
+}
