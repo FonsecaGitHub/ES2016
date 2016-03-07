10c6d06ed3202e07b4aa9faee14a7046ebb6a2c4
joaofeneja <joaofeneja@gmail.com>
2016-03-07 21:37:37 +0000

    app and dir unused and remover

--- src/main/java/pt/tecnico/mydrive/domain/App.java
+++
@@ -1,7 +0,0 @@
-package pt.tecnico.mydrive.domain;
-
-import pt.tecnico.mydrive.domain.File;
-
-public class App extends App_Base {
-	
-}
--- src/main/java/pt/tecnico/mydrive/domain/Dir.java
+++
@@ -1,7 +0,0 @@
-package pt.tecnico.mydrive.domain;
-
-import pt.tecnico.mydrive.domain.File;
-
-public class Dir extends Dir_Base {
-	
-}
