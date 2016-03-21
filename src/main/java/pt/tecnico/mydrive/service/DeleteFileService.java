42e56afa63e57f881d761f0c5221f48ec0f567dc
joaofeneja <joaofeneja@gmail.com>
2016-03-21 15:38:50 +0000

    Services arranged

---
+++ src/main/java/pt/tecnico/mydrive/service/DeleteFileService.java
@@ -0,0 +1,18 @@
+package pt.tecnico.mydrive.service;
+
+import pt.tecnico.mydrive.exception.MyDriveException;
+
+//import pt.tecnico.mydrive.exception.*;
+//import pt.tecnico.mydrive.domain.*;
+
+
+public class DeleteFileService extends MyDriveService{
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
