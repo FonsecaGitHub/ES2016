689c7ff819733186c894b38cab0491b1d2ff87f1
joaofeneja <joaofeneja@gmail.com>
2016-03-21 16:27:42 +0000

    Tests structured; review phonebook

---
+++ src/test/java/pt/tecnico/mydrive/service/ChangeDirectoryTest.java
@@ -0,0 +1,23 @@
+package pt.tecnico.mydrive.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.exception.*;
+
+public class ChangeDirectoryTest extends AbstractServiceTest {
+
+    protected void populate() {
+        
+    }
+
+   
+
+    @Test
+    public void success() {
+       
+    }
+}
