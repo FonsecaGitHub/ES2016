41a1b06f760e006faee31742f3b1852f247b4f1a
joaofeneja <joaofeneja@gmail.com>
2016-04-05 08:40:29 +0100

    list services

--- src/main/java/pt/tecnico/mydrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/mydrive/service/ListDirectoryService.java
@@ -3,11 +3,12 @@ package pt.tecnico.mydrive.service;
 import pt.tecnico.mydrive.exception.MyDriveException;
 
 //import pt.tecnico.mydrive.exception.*;
-//import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.domain.*;
 
 
 public class ListDirectoryService extends MyDriveService{
 
+	
 	public ListDirectoryService(long token) {
 		super.userToken = token;
 		
@@ -16,6 +17,8 @@ public class ListDirectoryService extends MyDriveService{
 	@Override
 	protected void dispatch() throws MyDriveException {
 		// TODO Auto-generated method stub
+		MyDrive md = super.getMyDrive();
+		User u = md.getUserByToken(this.userToken);
 		
 	}
 	
