65dd75b261c7b00c1a8f53d233ce238f64440ef9
Gil <silva-gil@live.com.pt>
2016-04-13 00:29:55 +0100

    close #27

--- src/main/java/pt/tecnico/mydrive/service/ReadFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/ReadFileService.java
@@ -2,13 +2,14 @@ package pt.tecnico.mydrive.service;
 
 import pt.tecnico.mydrive.exception.MyDriveException;
 
-//import pt.tecnico.mydrive.exception.*;
-//import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.exception.*;
+import pt.tecnico.mydrive.domain.*;
 
 
 public class ReadFileService extends MyDriveService{
-/*
+
 	private String name;
+	private String cont;
 	
 	public ReadFileService(long userToken, String name){
 	this.userToken = userToken;
@@ -16,13 +17,14 @@ public class ReadFileService extends MyDriveService{
 	
 	}
 	
-	
-*/	
 	@Override
 	protected void dispatch() throws MyDriveException {
-		// TODO Auto-generated method stub
-		
+		MyDrive md = MyDrive.getInstance();
+		PlainFile pf = md.getPlainFileByName(name);
+		cont = pf.getContent();
 	}
-	
+	public final String getCont(){
+		return cont; 
+	}
 	
 }
