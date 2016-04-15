c211e272770a628f20b0b992e8ebc533f2ad7351
sikrew <gil@gilgomes.com>
2016-04-15 17:22:56 +0100

    closes #26

--- src/main/java/pt/tecnico/mydrive/service/WriteFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/WriteFileService.java
@@ -1,6 +1,7 @@
 package pt.tecnico.mydrive.service;
 
-import pt.tecnico.mydrive.exception.MyDriveException;
+import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.exception.*;
 
 //import pt.tecnico.mydrive.exception.*;
 //import pt.tecnico.mydrive.domain.*;
@@ -8,10 +9,75 @@ import pt.tecnico.mydrive.exception.MyDriveException;
 
 public class WriteFileService extends MyDriveService{
 
-	@Override
-	protected void dispatch() throws MyDriveException {
-		// TODO Auto-generated method stub
-		
+	private int id;
+	private String name;
+	private String type;
+	private String content;
+
+	public WriteFileService(long userToken, String name, String type, String content) {
+		this.userToken = userToken;
+		this.name = name;
+		this.type = type;
+		this.content = content;
+	}
+
+
+	protected void dispatch() throws FileAlreadyExistsException, DirectoryDoesNotExistException, UserIsNotInSessionException, InvalidFileTypeException {
+		try {
+			MyDrive md = MyDrive.getInstance();
+			User user = md.getSession().getUserFromToken(userToken);
+			if(user == null || !md.getSession().inSession(user)) {
+				throw new UserIsNotInSessionException(userToken);
+			}
+
+			String tempPath = md.getDirectory().getPath();
+			if(tempPath != null)
+				throw new DirectoryDoesNotExistException(name);
+
+			File file = user.getFileByName(name);
+
+
+			//hmmm not the best approach...
+			PlainFile pf = md.getPlainFileByName(name);
+			String filetype = pf.getType();
+
+			switch (filetype) {
+				case "Plain File":
+					PlainFile plainFile = new PlainFile(md, name, content);
+					md.addFile(plainFile);
+					break;
+				case "Link":
+					Link link = new Link(content);
+					md.addFile(link);
+					break;
+				case "Directory":
+					Directory dir = new Directory(content, name);
+					md.addFile(dir);
+					break;
+				case "Application":
+					Application app = new Application(content);
+					md.addFile(app);
+					break;
+				default:  throw new InvalidFileTypeException(type);
+			}
+
+			user.removeFile(pf);
+
+
+		}
+		catch (FileAlreadyExistsException faee) {
+			faee.getMessage();
+		}
+		catch (DirectoryDoesNotExistException ddne) {
+			ddne.getMessage();
+		}
+		catch (InvalidFileTypeException ifte) {
+			ifte.getMessage();
+		}
+		catch (UserIsNotInSessionException uinise) {
+			uinise.getMessage();
+		}
+
 	}
 	
 	
