ba05e75d04eaaa89a080cedca159456b1f511631
FonsecaGitHub <miguel.fonseca1011011@gmail.com>
2016-04-22 22:05:44 +0100

    .

--- src/test/java/pt/tecnico/mydrive/service/CreateFileTest.java
+++ src/test/java/pt/tecnico/mydrive/service/CreateFileTest.java
@@ -1,7 +1,7 @@
 package pt.tecnico.mydrive.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
