4b4c7ed12c96148bfb181b3dbd683552f8b1ccb7
Miguel Francisco Vicente da Fonseca <ist167040@lab14p9.rnl.tecnico.ulisboa.pt>
2016-05-03 09:20:21 +0100

    SystemTest classes added, needs to be implemented

---
+++ src/test/java/pt/tecnico/mydrive/system/SystemTest.java
@@ -0,0 +1,32 @@
+ package pt.tecnico.mydrive.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+//TODO meter directoria dos testes
+//import pt.tecnico.mydrive.service.AbstractServiceTest ;
+import pt.tecnico.mydrive.presentation.*;
+
+public class SystemTest extends AbstractServiceTest {
+
+    private PbShell sh;
+
+    protected void populate() {
+        sh = new PbShell();
+    }
+
+    @Test
+    public void success() {
+        //exemplos do phonebook
+	/*
+	new Import(sh).execute(new String[] { "other.xml" } );
+        new CreatePerson(sh).execute(new String[] { "Rui" } );
+        new CreateContact(sh).execute(new String[] { "Rui", "SOS", "112" } );
+        new List(sh).execute(new String[] { } );
+        new RemoveContact(sh).execute(new String[] { "Xana", "Xico" } );
+        new RemovePerson(sh).execute(new String[] { "Sofia" } );
+        new Export(sh).execute(new String[] { } );
+	*/
+    }
+}
