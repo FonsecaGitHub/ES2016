fd5e2da089311eaf717ab68d98dc60b050b07049
Miguel Francisco Vicente da Fonseca <ist167040@lab14p9.rnl.tecnico.ulisboa.pt>
2016-05-03 09:30:03 +0100

    IntegrationTest update

--- src/test/java/pt/tecnico/mydrive/system/IntegrationTest.java
+++ src/test/java/pt/tecnico/mydrive/system/IntegrationTest.java
@@ -1,1 +1,21 @@
- package pt.tecnico.mydrive.integration;
+package pt.tecnico.mydrive.integration;
+ 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.integration.junit4.JMockit;
+
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+
+@RunWith(JMockit.class)
+public class IntegrationTest extends AbstractServiceTest {
+	private static final List<String> names = new ArrayList<String>();
+	private static final String u1 = "Miguel", u2 = "Vasconcelos", u3 = "Fenjea", u4 = "Silva", u5 = "Kinas", u6 = "Gomes";
+	private static final String importFile = "other.xml";
+}
