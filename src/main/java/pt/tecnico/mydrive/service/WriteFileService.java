f52e7fabad1b9a309a9688cffa00f594ade989a5
FonsecaGitHub <miguel.fonseca1011011@gmail.com>
2016-05-08 19:51:09 +0100

    WriteFileService receives path

--- src/main/java/pt/tecnico/mydrive/service/WriteFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/WriteFileService.java
@@ -3,14 +3,8 @@ package pt.tecnico.mydrive.service;
 import pt.tecnico.mydrive.domain.*;
 import pt.tecnico.mydrive.exception.*;
 
-//import pt.tecnico.mydrive.exception.*;
-//import pt.tecnico.mydrive.domain.*;
-
-
 public class WriteFileService extends MyDriveService{
 
-	//private int id;
-	private String name;
 	private String type;
 	private String content;
 	private String path;
@@ -22,10 +16,9 @@ public class WriteFileService extends MyDriveService{
 	 * @param name
 	 * @param content
 	 */
-	//TODO em vez de um name passa a receber um path
-	public WriteFileService(long userToken, String name, String content) {
+	public WriteFileService(long userToken, String path, String content) {
 		this.userToken = userToken;
-		this.name = name;
+		this.path = path;
 		this.content = content;
 	}
 
@@ -33,6 +26,11 @@ public class WriteFileService extends MyDriveService{
 		try {
 			MyDrive md = MyDrive.getInstance();
 			User user = md.getSession().getUserFromToken(userToken);
+			
+			String filenameWithPath = path;
+			String[] tokens = filenameWithPath.split("[\\\\|/]"); 
+			String name = tokens[tokens.length - 1];
+			
 			if(user == null || !md.getSession().inSession(user)) {
 				throw new UserIsNotInSessionException(userToken);
 			}
