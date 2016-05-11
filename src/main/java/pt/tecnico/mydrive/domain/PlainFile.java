43f79d4af8bf48f8a4a549ad76bd807d85c7a849
joaofeneja <joaofeneja@gmail.com>
2016-05-11 17:19:55 +0100

    my work is done my teammates. good luck to you all

--- src/main/java/pt/tecnico/mydrive/domain/PlainFile.java
+++ src/main/java/pt/tecnico/mydrive/domain/PlainFile.java
@@ -1,7 +1,6 @@
 package pt.tecnico.mydrive.domain;
 
 import java.io.*;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,84 +8,76 @@ import java.util.Scanner;
 import pt.tecnico.mydrive.domain.MyDrive;
 import pt.tecnico.mydrive.exception.UserIsNotInSessionException;
 
-
 public class PlainFile extends PlainFile_Base {
-	
+
 	public PlainFile(MyDrive md) {
 		super();
 		setId(md.incNumberOfFiles());
 		setMydrive(md);
 	}
-    
-    public PlainFile(MyDrive root, String name, String content) {
-        this(root);
-        setName(name);
-        setContent(content);
-    }
 
-    public PlainFile(){
-        super();
-    }
+	public PlainFile(MyDrive root, String name, String content) {
+		this(root);
+		setName(name);
+		setContent(content);
+	}
 
-    public PlainFile(String filepath, String filename){
-        super();
-    }
-    
-    public void delete() {
-    	//TODO ver se e preciso apagar o que esta pra tras
-    	setOwner(null);
-    	setMydrive(null);
-    	deleteDomainObject();
-    }
-    
-    //TODO Gil e isto que e preciso fazer
-    /*
-    public Element exportToXML() {
-    	Element pf_element = new Element("PlainFile");
-    	pf_element.setAttribute("_id", Integer.toString(getId()));
-    	//...
-    }*/
-    
-    
-    public void readFile(String path) throws FileNotFoundException, IOException{
-    	BufferedReader br = new BufferedReader(new FileReader(path));
-        try {
-            for (String line; (line = br.readLine()) != null; ) {
-                System.out.println(line);
-            }
-        }
-        catch(FileNotFoundException e){
-            //e.printStackTrace();
-            System.out.println(e.toString());
-        }
-        catch(IOException e){
-            //e.printStackTrace();
-            System.out.println(e.toString());
-        }
-    }
+	public PlainFile() {
+		super();
+	}
 
+	public PlainFile(String filepath, String filename) {
+		super();
+	}
 
-    public void writeListOfUsers(String fileName) {
-        //TODO
-    }
-    
-    
-    public void doExecute(User u, String programName, List<String> args){
+	public void delete() {
+		// TODO ver se e preciso apagar o que esta pra tras
+		setOwner(null);
+		setMydrive(null);
+		deleteDomainObject();
+	}
+
+	// TODO Gil e isto que e preciso fazer
+	/*
+	 * public Element exportToXML() { Element pf_element = new
+	 * Element("PlainFile"); pf_element.setAttribute("_id",
+	 * Integer.toString(getId())); //... }
+	 */
+
+	public void readFile(String path) throws FileNotFoundException, IOException {
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		try {
+			for (String line; (line = br.readLine()) != null;) {
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			// e.printStackTrace();
+			System.out.println(e.toString());
+		} catch (IOException e) {
+			// e.printStackTrace();
+			System.out.println(e.toString());
+		}
+	}
+
+	public void writeListOfUsers(String fileName) {
+		// TODO
+	}
+
+	public void doExecute(User u, List<String> args) throws IOException {
+		PrintWriter out = new PrintWriter(System.out, true);
 		if (!u.isInSession()) {
 			throw new UserIsNotInSessionException(u.getMyToken());
 		}
-		/*String input;
+		String input;
 		Thread master = Thread.currentThread();
 		Scanner scan = new Scanner(System.in);
 
 		ProcessBuilder builder;
 		if (args.size() == 0)
-			builder = new ProcessBuilder(args);
+			builder = new ProcessBuilder("/bin/bash");
 		else {
-			java.util.List<String> l = new ArrayList<String>();
-			for (String s : args)
-				l.add(s);
-			builder = new ProcessBuilder(l);
+			
+			builder = new ProcessBuilder(args);
 		}
 		builder.redirectErrorStream(true);
 		Process proc = builder.start();
@@ -96,20 +87,28 @@ public class PlainFile extends PlainFile_Base {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
 
+		Thread throut = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String line;
+				try {
+					while ((line = reader.readLine()) != null) {
+						out.println("Stdout: " + line);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				System.err.println("Stdout is now closed!!!");
+			}
+		});
+		throut.start();
 
-	    Thread throut = new Thread(new Runnable() {
-	    @Override
-	    public void run() {
-	    	String line;
-	    	try {
-	    		while ((line = reader.readLine ()) != null) {
-	    			out.println ("Stdout: " + line);
-	    		}
-	    	} catch (IOException e) { e.printStackTrace(); }
-	    		System.err.println ("Stdout is now closed!!!");
-	      	}
-	    }
-
+		/*
+		 * java 1.7 begin (must add an addition \n at the end) if ((input =
+		 * scan.nextLine()) != null) { writer.write(input); writer.newLine();
+		 * writer.flush(); } java 1.7 end
+		 */
+		/* java 1.8 begin */
 		for (;;) {
 			do
 				try {
@@ -126,30 +125,38 @@ public class PlainFile extends PlainFile_Base {
 			} else
 				break;
 		}
-	
+		/* java 1.8 end */
 		try {
 			proc.waitFor();
-		} catch (InterruptedException e) {}
+		} catch (InterruptedException e) {
+		}
 
 		System.err.println("exit: " + proc.exitValue());
-		proc.destroy();*/
+		proc.destroy();
 	}
-    
-    @Override
-	public void execute(User u,  List<String> args) {
+
+	@Override
+	public void execute(User u, List<String> args) {
 		if (getContent() != null) {
 			String[] arguments = getContent().split("\n");
 			for (int i = 0; i < arguments.length; i++) {
 				String[] a = arguments[i].split(" ");
-				//doExecute(u,a[0],a);
+				List<String> list = new ArrayList<String>();
+				for(int j = 0; j< a.length;j++){
+					list.add(a[j]);
+				}
+				list.addAll(args);
+				try {
+					doExecute(u, list);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
 
-	
-    
-    @Override
-    public String getType(){
-    	return "Plain File";
-    }
+	@Override
+	public String getType() {
+		return "Plain File";
+	}
 }
