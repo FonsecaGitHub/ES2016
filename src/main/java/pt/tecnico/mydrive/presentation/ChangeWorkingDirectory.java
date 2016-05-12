6e05c9a9721e36e505b41c75960c3d7fc3392be2
Gil <silva-gil@live.com.pt>
2016-05-12 04:29:19 +0100

    changedirectory changes

--- src/main/java/pt/tecnico/mydrive/presentation/ChangeWorkingDirectory.java
+++ src/main/java/pt/tecnico/mydrive/presentation/ChangeWorkingDirectory.java
@@ -1,10 +1,13 @@
 package pt.tecnico.mydrive.presentation;
 
+import pt.tecnico.mydrive.service.ChangeDirectoryService;
+
+
 public class ChangeWorkingDirectory extends MdCommand {
 
 	//TODO
     public ChangeWorkingDirectory(Shell sh) {
-    	super(sh, "list", "list persons (or person contacts, given person name");
+    	super(sh, "changedir", "change working directory");
     }
     
     /**
@@ -13,7 +16,17 @@ public class ChangeWorkingDirectory extends MdCommand {
      * 
      * cwd [path]
      */
-    public void execute(String[] args) {
-	//TODO
+    public void execute(String[] args) { /*
+    ChangeDirectoryService cwd; 
+    long token = ((MdShell) shell()).getUserToken();     
+    
+    if(args.length < 1)
+        throw new RuntimeException("USAGE: "+name()+" <path> ");
+       cwd =  new ChangeDirectoryService(token, args[0]);
+       */
+       //execute
+       //imprimir nova directoria
+       //cwd.execute();
+    	//System.out.println(
     }
 }
