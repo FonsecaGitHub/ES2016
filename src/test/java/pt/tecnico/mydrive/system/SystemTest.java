c12d8223daf8be4cf0da7b2f13483422d7a0af38
tiago santos <tiagokinasmiranda@gmail.com>
2016-05-12 18:04:41 +0100

    system test updated

--- src/test/java/pt/tecnico/mydrive/system/SystemTest.java
+++ src/test/java/pt/tecnico/mydrive/system/SystemTest.java
@@ -16,13 +16,23 @@ public class SystemTest extends AbstractServiceTest {
     @Test
     public void success() {
     	//new Import(sh).execute(new String[] { "other.xml" } );
+    	
+    	//new Login(sh).execute(new String[] {} )
         new Login(sh).execute(new String[] { "Miguel", "password" } );
-        new Key(sh).execute(new String[] { } );
+        //new Key(sh).execute(new String[] { } );
         new Key(sh).execute(new String[] { "Joao", "iloveist" } );
-        //new ChangeWorkingDirectory(sh).execute(new String[] { "/home/test2" } );
-        new List(sh).execute(new String[] { } );
+        //new ChangeWorkingDirectory(sh).execute(new String[] {} )
+        new ChangeWorkingDirectory(sh).execute(new String[] { "/home/test2" } );
+        //new List(sh).execute(new String[] { } );
+        new List(sh).execute(new String[] {"/home/test"} );
+        //new Write(sh).execute(new String[] { } );
+        new Write(sh).execute(new String[] { "/home/test/file.txt"} );
         new Write(sh).execute(new String[] { "/home/test/file.txt", "Hello World" } );
         //new Environment(sh).execute(new String[] { } );
+        new Environment(sh).execute(new String[] {"cenas","/home/test/"} );
+        //new Environment(sh).execute(new String[] { "cenas" } );
+        //new Execute(sh).execute(new String[] {} );
+        new Execute(sh).execute(new String[] {"/home/test/","cd"} );
         
 	
     }
